435f3cfd2185cb552d5e0cbb77065988a6f911f3
FJSDS <Fengjing520!>
2024-02-01 11:00:20 +0800

    update

--- builder.go
+++ builder.go
@@ -6,37 +6,41 @@ import (
 	"unsafe"
 )
 
-type Builder struct {
+type d struct {
 	data []byte
 }
 
+type Builder struct {
+	d *d
+}
+
 func (this_ *Builder) String() string {
-	return unsafe.String(unsafe.SliceData(this_.data), len(this_.data))
+	return unsafe.String(unsafe.SliceData(this_.d.data), len(this_.d.data))
 }
 
 func (this_ *Builder) Len() int {
-	return len(this_.data)
+	return len(this_.d.data)
 }
 
 func (this_ *Builder) Cap() int {
-	return cap(this_.data)
+	return cap(this_.d.data)
 }
 
 func (this_ *Builder) Reset() {
-	this_.data = this_.data[:0]
+	this_.d.data = this_.d.data[:0]
 }
 
 func (this_ *Builder) grow() {
-	if cap(this_.data) == 0 {
-		this_.data = GetSample(40960)[:0]
-		runtime.SetFinalizer(&this_.data, func(d *[]byte) {
-			PutSample(*d)
+	if cap(this_.d.data) == 0 {
+		this_.d.data = GetSample(40960)[:0]
+		runtime.SetFinalizer(&this_.d, func(d *d) {
+			PutSample(d.data)
 		})
 	} else {
-		tmp := GetSample(cap(this_.data) * 2)
-		copy(tmp, this_.data)
-		PutSample(this_.data)
-		this_.data = tmp
+		tmp := GetSample(cap(this_.d.data) * 2)
+		copy(tmp, this_.d.data)
+		PutSample(this_.d.data)
+		this_.d.data = tmp
 	}
 }
 
@@ -44,32 +48,35 @@ func (this_ *Builder) Grow(n int) {
 	if n < 0 {
 		panic("strings.Builder.Grow: negative count")
 	}
-	if cap(this_.data)-len(this_.data) < n {
+	if this_.d == nil {
+		this_.d = &d{}
+	}
+	if cap(this_.d.data)-len(this_.d.data) < n {
 		this_.grow()
 	}
 }
 
 func (this_ *Builder) Write(p []byte) (int, error) {
 	this_.Grow(len(p))
-	this_.data = append(this_.data, p...)
+	this_.d.data = append(this_.d.data, p...)
 	return len(p), nil
 }
 
 func (this_ *Builder) WriteByte(c byte) error {
 	this_.Grow(1)
-	this_.data = append(this_.data, c)
+	this_.d.data = append(this_.d.data, c)
 	return nil
 }
 
 func (this_ *Builder) WriteRune(r rune) (int, error) {
 	this_.Grow(4)
-	n := len(this_.data)
-	this_.data = utf8.AppendRune(this_.data, r)
-	return len(this_.data) - n, nil
+	n := len(this_.d.data)
+	this_.d.data = utf8.AppendRune(this_.d.data, r)
+	return len(this_.d.data) - n, nil
 }
 
 func (this_ *Builder) WriteString(s string) (int, error) {
 	this_.Grow(len(s))
-	this_.data = append(this_.data, s...)
+	this_.d.data = append(this_.d.data, s...)
 	return len(s), nil
 }
